158038d52fbe443a88c56125c46f36e719842f63
Olav <olav@schettler.net>
2011-07-04 00:09:47 +0200

    Topic photos float left; photo upload form folded

--- views/user_login.php
+++ views/user_login.php
@@ -1,6 +1,6 @@
 <form id="login" method="POST">
-  <label for="mail">E-Mail-Adresse</label><input type="email" name="mail" value="" id="">
-  <label for="password">Kennwort</label><input type="password" name="password" value="" id="">
+  <label for="mail">E-Mail-Adresse</label><input class="input" type="email" name="mail" value="" id="">
+  <label for="password">Kennwort</label><input class="input" type="password" name="password" value="" id="">
   
   <input type="submit" name="op" value="Anmelden" id="some_name">
   
