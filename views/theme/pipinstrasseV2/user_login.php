7d46b585d72eb2898585716e773af68cf4c84766
Frederic <frederic@biberalarm.de>
2011-07-11 21:17:24 +0200

    Verbesserte Login-Seite

--- views/theme/pipinstrasseV2/user_login.php
+++ views/theme/pipinstrasseV2/user_login.php
@@ -1,3 +1,18 @@
+<h1>In nur drei Schritten</h1>
+<p> 
+Wenn Sie <b>neu</b> hier sind, dann klicken Sie bitte auf "<a href="/user/register">Nutzerkonto anmelden</a>". Dort füllen Sie dann das Formular aus und klicken am Ende der Seite auf "Änderungen speichern". Sie landen anschließend wieder auf der Startseite. Nun prüfen wir die Anmeldung und schalten Sie frei. Wenn Sie freigeschaltet sind, erhalten Sie von uns eine E-Mail. Anschließend können Sie sich unten mit E-Mail-Adresse und Kennwort anmelden
+</p>              
+<p>
+  Wenn Sie schon registriert sind und die Freischaltungs-E-Mail bekommen haben, dann geben Sie unten einfach Ihre E-Mail-Adresse an und das Kennwort welches Sie bei der Registration angegeben haben.
+</p> 
+<p>
+  Bei Fragen können Sie uns über die <a href="/message/contact">Kontaktseite</a> erreichen.
+</p>
+
+
+<br />
+<hr />
+<br />
 <h1>Login</h1>
 <form id="login" method="POST">
   <label for="mail">E-Mail-Adresse</label><input class="input" type="email" name="mail" value="" id=""> <br />
