8ac284cbe962808c13f8d54295058024f8391691
Frederic <frederic@biberalarm.de>
2011-07-12 00:13:38 +0200

    alten themes Ordner löschen

--- views/theme/pipinstrasseV2/home_index.php
+++
@@ -1,3 +0,0 @@
-<h2>Willkommen</h2>
-<p>Auf diesen Seiten möchten wir es Ihnen ermöglichen, sich auszutauschen. Schauen Sie sich einfach einmal um!</p>
-<p><br /><b>Bitte beachten Sie, dass diese Seite noch im Aufbau ist, denn leider sind nicht alle Bereiche rechtzeitig fertig geworden.</b></p>
