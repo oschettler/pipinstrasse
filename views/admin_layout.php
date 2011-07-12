917cdabb975a4e0f66cdcb4f264abb34a10a8625
Olav <olav@schettler.net>
2011-07-12 21:00:27 +0200

    Admin styling

--- views/admin_layout.php
+++ views/admin_layout.php
@@ -7,6 +7,16 @@
   <style type="text/css" media="screen">
   body {
     font-family: arial, helvetica, sans-serif;
+    background-color: #EEE;
+    padding: 0;
+    margin: 0;
+  }
+  
+  #wrapper {
+    width: 940px;
+    margin: auto;
+    background-color: white;
+    padding: 10px;
   }
 
   tr:nth-child(even) { background-color: #EEE }
@@ -30,6 +40,46 @@
     background: #FCC;
   }
   
+  #nav {
+    position: relative;
+    margin: 20px 10px;
+  }
+  
+  #nav ul {
+    list-style-type: none;
+    display: none;
+    position: absolute;
+    top: 24px;
+    left: 4px;
+    padding: 10px;
+    background-color: #888;
+    border: solid 2px white;
+  }
+
+  #nav a#top {
+    background-color: #888;
+    border: solid 2px white;
+    color: white;
+    text-decoration: none;
+    padding: 10px;
+  }
+  
+  #nav ul {
+    margin: 0;
+    padding: 0;
+  }
+  
+  #nav ul a {
+    color: white;
+    text-decoration: none;
+    display: block;
+    padding: 4px;
+  }
+  
+  #nav ul a:hover {
+    background-color: #444;
+  }
+  
   textarea {
     display: block;
     width: 100%;
@@ -43,13 +93,24 @@
     echo join("\n", $page_head);
   }
   ?>
+  <script type="text/javascript" charset="utf-8">
+    jQuery(function($) {
+      $('#nav > a').toggle(function() {
+        $(this).next('ul').slideDown('fast');
+        return false;
+      }, function() {
+        $(this).next('ul').slideUp('fast');
+        return false;
+      });
+    });
+  </script>
 </head>
 <body id="admin">
   <?php
   if ($msg = $this->message()) {
     echo "<div class=\"message {$msg['class']}\">{$msg['text']}</div>\n";
   }
-  echo $this->render('_nav', array('navigation' => array(
+  echo '<div id="nav"><a id="top" href="#">Verwaltung</a>', $this->render('_nav', array('navigation' => array(
     '/' => 'Frontend',
     '/admin' => 'Admin Startseite',
     '/admin/users' => 'Nutzer verwalten',
@@ -58,10 +119,11 @@
     // 'http://schettler.net/fossil/pipinstrasse' => 'Fossil Repository',
     '/admin/migrate' => array('confirm' => TRUE, 'Datenbank aktualisieren'),
     // '/admin/move_photos' => array('confirm' => TRUE, 'Fotos verschieben'),
-  )));
-  
+  ))), '</div><!--#nav-->';
   ?>
-  <h1><?php echo $title; ?></h1>
-  <?php echo $contents; ?>
+  <div id="wrapper">
+    <h1><?php echo $title; ?></h1>
+    <?php echo $contents; ?>
+  </div><!--#wrapper-->
 </body>
 </html>
