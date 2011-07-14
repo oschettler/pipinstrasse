dc59afdaf4cd18d42dd03c7e4c0a8d87e0ee597b
Olav <olav@schettler.net>
2011-07-14 11:44:26 +0200

    Fix

--- www/deploy/deploy-cronjob.sh
+++ www/deploy/deploy-cronjob.sh
@@ -3,7 +3,7 @@ script=/tmp/deploy-pipinstrasse.sh
 log=/tmp/deploy-pipinstrasse.log
 
 if [ -f $script ]; then
-  grep -e '#since' >> $log
+  grep -e '#since' $script >> $log
   sh -x $script >> $log 2>&1
   date '+#done %Y-%m-%d %H:%M:%S' >> $log
 
